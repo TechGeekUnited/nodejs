d99f217d4303f380c4176ea5627d0abac168ea9a
Tech Geek United <79381496+TechGeekUnited@users.noreply.github.com>
2023-06-12 13:12:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const http = require('http');
-const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3004;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  console.log(`Request for ${req.url} received.`);
+
+  // Serve static HTML files
+  if (req.method === 'GET' && req.url.endsWith('.html')) {
+    const filePath = path.join(__dirname, 'public', req.url);
+    serveFile(filePath, 'text/html', res);
+  }
+ // Serve static HTML files
+  if (req.method === 'GET' && req.url.endsWith('.png')) {
+    const filePath = path.join(__dirname, 'public', req.url);
+    serveFile(filePath, 'text/html', res);
+  }
+  // Check for index.html in directories
+  else if (req.method === 'GET' && !path.extname(req.url)) {
+    const filePath = path.join(__dirname, 'public', req.url, 'index.html');
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        const errorFilePath = path.join(__dirname, 'public', '404', '404.html');
+        serveFile(errorFilePath, 'text/html', res, 404);
+      } else {
+        serveFile(filePath, 'text/html', res);
+      }
+    });
+  }
+
+  // Handle 404 error
+  else {
+    const errorFilePath = path.join(__dirname, 'public', '404', '404.html');
+    serveFile(errorFilePath, 'text/html', res, 404);
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+function serveFile(filePath, contentType, res, statusCode = 200) {
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(`Error reading file: ${err}`);
+    } else {
+      res.writeHead(statusCode, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+}
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
